ddd0371ce53dee19f55fd7024c16056d35911263
Caitlin Majewski <cmajewski@wesleyan.edu>
2021-05-26 18:10:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,11 +1,20 @@
 class Artist
   attr_accessor :name, :songs
-
+  @@all_songs=[]
   def initialize (name)
     @name=name
   end
 
   def add_song(song)
-    songs<<song
+    @songs<<song
+    @@all_songs<<song
+  end
+
+  def add_song_by_name
+
+  end
+
+  def self.song_count
+    @@all_songs.size
   end
 end
